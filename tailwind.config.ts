44b38e416938cebe912e4a5691866dfb6b9e7c8b
miguel-willians <miiguel.wso@gmail.com>
2025-02-25 16:29:43 -0300

    chore: ajustar as configurações de rota do Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,9 @@ import type { Config } from "tailwindcss";
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{js,ts,jsx,tsx}", // Incluir a pasta features
+    "./src/ui/**/*.{js,ts,jsx,tsx}", // Incluir a pasta ui
   ],
   theme: {
     extend: {
