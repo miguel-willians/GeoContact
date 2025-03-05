31f811fbc3a88a9d0a254de927813d989b29bc7a
miguel-willians <miiguel.wso@gmail.com>
2025-03-05 16:51:23 -0300

    style: criar novos estilos para o ButtonIcon e adaptar os existentes

--- src/features/user/User.tsx
+++ src/features/user/User.tsx
@@ -1,15 +1,13 @@
-import Link from "next/link";
+import ButtonIcon from "@/ui/ButtonIcon";
+import { ImExit } from "react-icons/im";
 
 export default function User() {
   return (
-    <div className="absolute top-[4.2rem] right-[4.2rem] bg-gray-900 p-[1rem_1.4rem] rounded-lg z-[999] shadow-[0_0.8rem_2.4rem_rgba(36,42,46,0.5)] text-[1.6rem] font-semibold flex items-center gap-4">
+    <div className="absolute top-[4.2rem] right-[4.2rem] bg-blue-900 p-[1rem_1.4rem] rounded-lg z-[999] shadow-[0_0.8rem_2.4rem_rgba(36,42,46,0.5)] text-md font-semibold flex items-center gap-4">
       <span>Welcome, USER</span>
-      <Link
-        href="/"
-        className="bg-gray-800 rounded-lg border-none px-3 py-1.5 text-inherit font-inherit text-[1.2rem] font-bold uppercase cursor-pointer"
-      >
-        Logout
-      </Link>
+      <ButtonIcon variant="primary">
+        <ImExit />
+      </ButtonIcon>
     </div>
   );
 }
--- src/ui/ButtonIcon.tsx
+++ src/ui/ButtonIcon.tsx
@@ -1,11 +1,19 @@
 import { ButtonHTMLAttributes } from "react";
 
-function ButtonIcon(props: ButtonHTMLAttributes<HTMLButtonElement>) {
+type ButtonIconProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?: "search" | "primary";
+};
+
+function ButtonIcon({ variant, ...props }: ButtonIconProps) {
+  const styles: Record<string, string> = {
+    search:
+      "p-2 rounded-r-lg rounded-sm bg-blue-400 hover:bg-blue-200 flex justify-center items-center",
+    primary:
+      "p-2 rounded-md bg-blue-950 hover:bg-blue-600 flex justify-center items-center",
+  };
+
   return (
-    <button
-      className="p-2 rounded-r-lg rounded-sm transition-all bg-blue-400 hover:bg-blue-200 flex justify-center items-center"
-      {...props}
-    >
+    <button className={variant ? styles[variant] : ""} {...props}>
       {props.children}
     </button>
   );
